7efc7d3ecbb1ae7dda67eb032c8c4110308e1130
Sanjana <97579455+kitty57@users.noreply.github.com>
2024-03-29 12:42:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,14 @@ def get_user_profile():
     reading_level_options = ["Preschool", "Elementary (K-2)", "Elementary (3-5)"]
     selected_reading_level = st.selectbox("Select your child's reading level:", reading_level_options)
     interests = st.text_input("What is your child interested in? (Separate interests with commas):")
-    character = st.selectbox("Choose your favorite character:", ["Twinkle", "Maya", "Robin", "Trio"])
+    characters = {
+        "Twinkle":"https://th.bing.com/th/id/OIP.wO1fi5o9K_cuTqKoEkylKgAAAA?rs=1&pid=ImgDetMain",
+        "Maya":"https://th.bing.com/th/id/OIP.3mHNwJMuJEd5O7UGv0HW5gHaNK?rs=1&pid=ImgDetMain",
+        "Tia":"https://th.bing.com/th/id/OIP.aCDDfBfOVZ5ggZC5sgvd3wHaHa?rs=1&pid=ImgDetMain",
+        "lily":"https://img.freepik.com/premium-photo/cute-girl-happy-cartoon-character_74102-2378.jpg"
+    }
+    
+    character = st.selectbox("Choose your favorite character:", list(characters.keys()), format_func=lambda x: f'<img src="{characters[x]}" style="width:100px;"> {x}', help="Click on the character to select")
     if selected_reading_level and interests and character:
         return {"reading_level": selected_reading_level, "interests": interests.split(","), "character": character.lower()}
     else:
@@ -20,7 +27,7 @@ def generate_story(user_profile):
     character = user_profile["character"]
     prompt = f"""
     Generate a story suitable for a {reading_level} year old child 
-    who is interested in {interests} with a character named {character}.
+    who is interested in {interests} with the protoganist being a girl named {character}.
     """
     story_segment = model.generate_content(prompt)
     return story_segment
