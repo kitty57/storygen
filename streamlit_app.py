62fa767fe3c2e3717fe1f71ba0326a5a811b4797
Sanjana <97579455+kitty57@users.noreply.github.com>
2024-03-29 11:46:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,29 +29,16 @@ def main():
     st.title("Come and listen to Amazing story about the character you choose!!!")
     user_profile = get_user_profile()
     if user_profile:
-        story_generated = False 
-        iteration_count = 0 
-        while True:
-            if not story_generated:
-                iteration_count += 1
-                generate_story_button_key = f"generate_story_button_{iteration_count}"
+        if st.button("Give me a story!"):
+            story_segment = generate_story(user_profile)
+            st.write(story_segment.text)
 
-                if st.button("Give me a story!", key=generate_story_button_key):
-                    story_segment = generate_story(user_profile)
-                    st.write(story_segment.text)
-                    story_generated = True 
+            choice_radio = st.radio("What would you like to do next?", ("I want to listen more!", "I'm bored, I want to quit"))
+            if choice_radio == "I want to listen more!":
+                pass  # Do nothing, allowing the user to generate more stories
+            elif choice_radio == "I'm bored, I want to quit":
+                st.write("You've chosen to stop listening to stories! Come back again to listen to more!")
 
-            if story_generated:
-                choice_radio_key = f"choice_radio_{iteration_count}"
-                choice = st.radio("What would you like to do next?", ("I want to listen more!", "I'm bored, I want to quit"), key=choice_radio_key)
-                if choice == "I want to listen more!":
-                    story_generated = False  
-                    continue  
-                elif choice == "I'm bored, I want to quit":
-                    st.write("You've chosen to stop listening to stories! Come back again to listen to more!")
-                    break  
-                else:
-                    st.warning("Please select an option.")
     else:
         st.warning("Please enter all the values for the user profile.")
 
